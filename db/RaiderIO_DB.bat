b5f0ed6f7890be86891a347ea2ed864b5cecc4be
Vladinator89 <vladix@gmail.com>
2022-06-18 10:25:24 +0200

    Loaded testing branch data and redid the folder generation script.

--- db/RaiderIO_DB.bat
+++ db/RaiderIO_DB.bat
@@ -1,97 +1,38 @@
 @echo off
 setlocal enabledelayedexpansion
+set cwd=%~dp0
 
-set toc_interface=90200
+:TOC
+set toc_interface=90205
 set toc_author=Vladinator ^(Vladinator-TarrenMill^), Aspyr ^(Aspyrox-Skullcrusher^) and Isak ^(Isak-Sargeras^)
 set toc_addon=RaiderIO
 
-:KeystoneAndRaidingPerRegionAndFaction
+:DB
 for %%r in (
-	"US	Americas"
+
 	"EU	Europe"
 	"KR	Korea"
 	"TW	Taiwan"
+	"US	Americas"
+
 ) do (
+
 	set r=%%r
 	set r1=!r:~1,2!
 	set r2=!r:~4,-1!
 
+	set lr=!r1!
+	call :LoCase lr
+	call :UnScor lr
+
 	for %%t in (
-		"R	Raiding"
+
 		"M	Mythic Plus"
-	) do (
-		set t=%%t
-		set t1=!t:~1,1!
-		set t2=!t:~3,-1!
-
-		set lt=!t2!
-		call :LoCase lt
-		call :UnScor lt
-
-		if !t1! neq M set t1=_!t1!
-		if !t1! equ M set t1=
-
-		if !lt! neq mythicplus set lt=_!lt!
-		if !lt! equ mythicplus set lt=
-
-		for %%f in (
-			"A	Alliance"
-			"H	Horde"
-		) do (
-			set f=%%f
-			set f1=!f:~1,1!
-			set f2=!f:~3,-1!
-
-			set d=.\RaiderIO_DB_!r1!_!f1!!t1!
-			set f=.\!d!\!d!.toc
-
-			set lr=!r1!
-			set lf=!f2!
-			call :LoCase lr
-			call :LoCase lf
-
-			echo !d! !f!
-
-			rmdir /s /q "!d!"
-			mkdir "!d!"
-
-			echo ## Interface: !toc_interface!>"!f!"
-			echo ## Title: Raider.IO ^|cffFFFFFF!t2!^|r ^(!r2! - !f2!^)>>"!f!"
-			echo ## Author: !toc_author!>>"!f!"
-			echo ## Dependencies: !toc_addon!>>"!f!"
-			echo ## DefaultState: enabled>>"!f!"
-			echo ## X-Type: !t2!>>"!f!"
-			echo ## X-Region: !r2!>>"!f!"
-			echo ## X-Faction: !f2!>>"!f!"
-			echo ## X-Website: https:^/^/raider.io>>"!f!"
-			if !t1! == _R (
-				echo ../!toc_addon!/db/db_raiding_!lr!_!lf!_characters.lua>>"!f!"
-				echo ../!toc_addon!/db/db_raiding_!lr!_!lf!_lookup.lua>>"!f!"
-			) else if !t1! == _F (
-				echo ../!toc_addon!/db/db_recruitment_!lr!_!lf!_characters.lua>>"!f!"
-				echo ../!toc_addon!/db/db_recruitment_!lr!_!lf!_lookup.lua>>"!f!"
-			) else (
-				echo ../!toc_addon!/db/db_!lr!_!lf!!lt!_characters.lua>>"!f!"
-				echo ../!toc_addon!/db/db_!lr!_!lf!!lt!_lookup.lua>>"!f!"
-			)
-		)
-	)
-)
-
-:RecruitmentPerRegionForBothFactions
-for %%r in (
-	"US	Americas"
-	"EU	Europe"
-	"KR	Korea"
-	"TW	Taiwan"
-) do (
-	set r=%%r
-	set r1=!r:~1,2!
-	set r2=!r:~4,-1!
-
-	for %%t in (
+		"R	Raiding"
 		"F	Recruitment"
+
 	) do (
+
 		set t=%%t
 		set t1=!t:~1,1!
 		set t2=!t:~3,-1!
@@ -100,51 +41,40 @@ for %%r in (
 		call :LoCase lt
 		call :UnScor lt
 
-		if !t1! neq M set t1=_!t1!
-		if !t1! equ M set t1=
+		set db1=db_!lt!_!lr!_characters.lua
+		set db2=db_!lt!_!lr!_lookup.lua
 
-		if !lt! neq mythicplus set lt=_!lt!
-		if !lt! equ mythicplus set lt=
+		if exist "!cwd!\!db1!" (
 
-		for %%f in (
-			"_	Alliance and Horde"
-		) do (
-			set f=%%f
-			set f1=!f:~1,1!
-			set f2=!f:~3,-1!
+			set d=RaiderIO_DB_!r1!_!t1!
+			set f=!d!\!d!.toc
 
-			set d=.\RaiderIO_DB_!r1!!t1!
-			set f=.\!d!\!d!.toc
+			set ad="!cwd!\!d!"
+			set af="!cwd!\!f!"
 
-			set lr=!r1!
-			set lf=!f2!
-			call :LoCase lr
-			call :LoCase lf
+			rmdir /s /q !ad!
+			mkdir !ad!
 
-			echo !d! !f!
+			echo ## Interface: !toc_interface!>!af!
+			echo ## Title: Raider.IO ^|cffFFFFFF!t2!^|r ^(!r2!^)>>!af!
+			echo ## Author: !toc_author!>>!af!
+			echo ## Dependencies: !toc_addon!>>!af!
+			echo ## DefaultState: enabled>>!af!
+			echo ## X-Type: !t2!>>!af!
+			echo ## X-Region: !r2!>>!af!
+			echo ## X-Faction: Both>>!af!
+			echo ## X-Website: https:^/^/raider.io>>!af!
+			echo ../!toc_addon!/db/!db1!>>!af!
+			echo ../!toc_addon!/db/!db2!>>!af!
 
-			rmdir /s /q "!d!"
-			mkdir "!d!"
-
-			echo ## Interface: !toc_interface!>"!f!"
-			echo ## Title: Raider.IO ^|cffFFFFFF!t2!^|r ^(!r2! - !f2!^)>>"!f!"
-			echo ## Author: !toc_author!>>"!f!"
-			echo ## Dependencies: !toc_addon!>>"!f!"
-			echo ## DefaultState: enabled>>"!f!"
-			echo ## X-Type: !t2!>>"!f!"
-			echo ## X-Region: !r2!>>"!f!"
-			echo ## X-Faction: !f2!>>"!f!"
-			echo ## X-Website: https:^/^/raider.io>>"!f!"
-			echo ../!toc_addon!/db/db_recruitment_!lr!_alliance_characters.lua>>"!f!"
-			echo ../!toc_addon!/db/db_recruitment_!lr!_alliance_lookup.lua>>"!f!"
-			echo ../!toc_addon!/db/db_recruitment_!lr!_horde_characters.lua>>"!f!"
-			echo ../!toc_addon!/db/db_recruitment_!lr!_horde_lookup.lua>>"!f!"
 		)
+
 	)
+
 )
 
-:SkipToEndOfFile
-goto End
+:GoToEnd
+GOTO:End
 
 REM http://www.robvanderwoude.com/battech_convertcase.php
 :LoCase
@@ -160,4 +90,4 @@ FOR %%i IN (" =") DO CALL SET "%1=%%%1:%%~i%%"
 GOTO:EOF
 
 :End
-pause
+REM pause
--- db/db_client_guilds.lua
+++ db/db_client_guilds.lua
@@ -1,6 +1,358 @@
--- __________        .__    .___           .___________   
--- \______   \_____  |__| __| _/___________|   \_____  \  
---  |       _/\__  \ |  |/ __ |/ __ \_  __ \   |/   |   \ 
---  |    |   \ / __ \|  / /_/ \  ___/|  | \/   /    |    \
---  |____|_  /(____  /__\____ |\___  >__|  |___\_______  /
---         \/      \/        \/    \/                  \/ 
+--
+-- RaiderIO Client Data. Generated on 2022-06-18T08:21:59Z
+--
+local _, ns = ...
+ns.GUILD_BEST_DATA = {
+  ["Overcharged-TarrenMill"] = {
+    ["profile"] = {
+      ["name"] = "Overcharged",
+      ["realm"] = "TarrenMill",
+      ["faction"] = "horde"
+    },
+    ["season_best"] = {
+      [1] = {
+        ["zone_id"] = 12916,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.862781981851912,
+        ["clear_time"] = "00:31:04.471",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Nirvîa",
+            ["role"] = "tank",
+            ["class_id"] = 10
+          },
+          [2] = {
+            ["name"] = "Elrack",
+            ["role"] = "healer",
+            ["class_id"] = 2
+          },
+          [3] = {
+            ["name"] = "Flup",
+            ["role"] = "dps",
+            ["class_id"] = 4
+          },
+          [4] = {
+            ["name"] = "Harmøwl",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [5] = {
+            ["name"] = "Rylahna",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          }
+        }
+      },
+      [2] = {
+        ["zone_id"] = 13228,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.906449323300887,
+        ["clear_time"] = "00:34:27.610",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Johnwæk",
+            ["role"] = "tank",
+            ["class_id"] = 12
+          },
+          [2] = {
+            ["name"] = "Mirr",
+            ["role"] = "healer",
+            ["class_id"] = 5
+          },
+          [3] = {
+            ["name"] = "Couroxz",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [4] = {
+            ["name"] = "Pasomonk",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          },
+          [5] = {
+            ["name"] = "Pejpros",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          }
+        }
+      },
+      [3] = {
+        ["zone_id"] = 12916,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9069425760955928,
+        ["clear_time"] = "00:32:39.902",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Flashtwo",
+            ["role"] = "tank",
+            ["class_id"] = 6
+          },
+          [2] = {
+            ["name"] = "Udyrmoche",
+            ["role"] = "healer",
+            ["class_id"] = 11
+          },
+          [3] = {
+            ["name"] = "Anderslock",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [4] = {
+            ["name"] = "Dexmoe",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [5] = {
+            ["name"] = "Flup",
+            ["role"] = "dps",
+            ["class_id"] = 4
+          }
+        }
+      },
+      [4] = {
+        ["zone_id"] = 13228,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9105681326471428,
+        ["clear_time"] = "00:34:37.005",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Thickster",
+            ["role"] = "tank",
+            ["class_id"] = 10
+          },
+          [2] = {
+            ["name"] = "Elfurious",
+            ["role"] = "healer",
+            ["class_id"] = 7
+          },
+          [3] = {
+            ["name"] = "Goobir",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [4] = {
+            ["name"] = "Sãn",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [5] = {
+            ["name"] = "Warcieft",
+            ["role"] = "dps",
+            ["class_id"] = 4
+          }
+        }
+      },
+      [5] = {
+        ["zone_id"] = 1000001,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9420727052041672,
+        ["clear_time"] = "00:28:16.672",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Kíea",
+            ["role"] = "tank",
+            ["class_id"] = 2
+          },
+          [2] = {
+            ["name"] = "Orcerfar",
+            ["role"] = "healer",
+            ["class_id"] = 7
+          },
+          [3] = {
+            ["name"] = "Flup",
+            ["role"] = "dps",
+            ["class_id"] = 4
+          },
+          [4] = {
+            ["name"] = "Magichuntard",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [5] = {
+            ["name"] = "Петятоплок",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          }
+        }
+      }
+    },
+    ["weekly_best"] = {
+      [1] = {
+        ["zone_id"] = 13228,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9761639527242231,
+        ["clear_time"] = "00:37:06.629",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Bryñhildr",
+            ["role"] = "tank",
+            ["class_id"] = 2
+          },
+          [2] = {
+            ["name"] = "Tinuninu",
+            ["role"] = "healer",
+            ["class_id"] = 5
+          },
+          [3] = {
+            ["name"] = "Bwinky",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [4] = {
+            ["name"] = "Suns",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          },
+          [5] = {
+            ["name"] = "Ÿä",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          }
+        }
+      },
+      [2] = {
+        ["zone_id"] = 1000001,
+        ["level"] = 26,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9905019380910262,
+        ["clear_time"] = "00:29:43.893",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Anajei",
+            ["role"] = "tank",
+            ["class_id"] = 12
+          },
+          [2] = {
+            ["name"] = "Notshieldyou",
+            ["role"] = "healer",
+            ["class_id"] = 5
+          },
+          [3] = {
+            ["name"] = "Makse",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [4] = {
+            ["name"] = "Spacew",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [5] = {
+            ["name"] = "Suns",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          }
+        }
+      },
+      [3] = {
+        ["zone_id"] = 12916,
+        ["level"] = 25,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.8591035905153126,
+        ["clear_time"] = "00:30:56.522",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Loathen",
+            ["role"] = "tank",
+            ["class_id"] = 12
+          },
+          [2] = {
+            ["name"] = "Ðéwy",
+            ["role"] = "healer",
+            ["class_id"] = 7
+          },
+          [3] = {
+            ["name"] = "Бирбой",
+            ["role"] = "dps",
+            ["class_id"] = 1
+          },
+          [4] = {
+            ["name"] = "Монторка",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          },
+          [5] = {
+            ["name"] = "Рудра",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          }
+        }
+      },
+      [4] = {
+        ["zone_id"] = 12916,
+        ["level"] = 25,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.881406238503581,
+        ["clear_time"] = "00:31:44.718",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Grippingdk",
+            ["role"] = "tank",
+            ["class_id"] = 6
+          },
+          [2] = {
+            ["name"] = "Adoreluna",
+            ["role"] = "healer",
+            ["class_id"] = 5
+          },
+          [3] = {
+            ["name"] = "Failgate",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [4] = {
+            ["name"] = "Feyangola",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [5] = {
+            ["name"] = "Suns",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          }
+        }
+      },
+      [5] = {
+        ["zone_id"] = 1000001,
+        ["level"] = 25,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.8828677861564609,
+        ["clear_time"] = "00:26:30.044",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Darodk",
+            ["role"] = "tank",
+            ["class_id"] = 6
+          },
+          [2] = {
+            ["name"] = "Elows",
+            ["role"] = "healer",
+            ["class_id"] = 7
+          },
+          [3] = {
+            ["name"] = "Joeytribiany",
+            ["role"] = "dps",
+            ["class_id"] = 9
+          },
+          [4] = {
+            ["name"] = "Pridehunterr",
+            ["role"] = "dps",
+            ["class_id"] = 3
+          },
+          [5] = {
+            ["name"] = "Suns",
+            ["role"] = "dps",
+            ["class_id"] = 10
+          }
+        }
+      }
+    }
+  }
+}
+

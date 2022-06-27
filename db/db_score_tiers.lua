b60b931c62d13e238357e15b960d909f94a56c45
Jah Raphael <jahraphael@yahoo.com>
2022-06-27 07:36:08 +0000

    [Raider.IO] Database Refresh

--- db/db_score_tiers.lua
+++ db/db_score_tiers.lua
@@ -1,5 +1,5 @@
 --
--- Generated on 2022-06-26T07:31:22Z. DO NOT EDIT.
+-- Generated on 2022-06-27T07:29:56Z. DO NOT EDIT.
 --
 -- Ranges: {"epic":[2876,4025],"superior":[2451,2875],"uncommon":[751,2450],"common":[200,750]}
 --

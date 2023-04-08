1edfc40186fff6ff5766a1a373ec4d7b189c15ba
Vladinator89 <vladix@gmail.com>
2023-04-08 17:31:22 +0200

    Patched the various adjustments from live to the export config. I should look into automating the zone id for the instances, currently we manually override the internal dummy counter as a solution.

--- tools/exportDungeonsData/export.js
+++ tools/exportDungeonsData/export.js
@@ -9,8 +9,23 @@ const config = {
     build: '10.1.0.48898',
     expansion: 9,
     season: 9,
-    // manually specify the activity id's for these particular keystone instances
+    // manually specify certain fields for these particular keystone instances
     keystoneInstanceOverride: {
+        [2]: { // Temple of the Jade Serpent
+            id: 5965,
+        },
+        [165]: { // Shadowmoon Burial Grounds
+            id: 6932,
+            lfd_activity_ids: [ 27, 35, 185, 407, 1193 ],
+        },
+        [200]: { // Halls of Valor
+            id: 7672,
+            shortName: 'HOV',
+        },
+        [210]: { // Court of Stars
+            id: 8079,
+            shortName: 'COS',
+        },
         [227]: { // Return to Karazhan: Lower
             lfd_activity_ids: [ 470, 471 ],
             name: 'Return to Karazhan: Lower',
@@ -38,6 +53,31 @@ const config = {
         [392]: { // Tazavesh: So'leah's Gambit
             lfd_activity_ids: [ 1017, 1019 ],
             shortName: 'GMBT',
+        },
+        [399]: { // Ruby Life Pools
+            id: 14063,
+        },
+        [400]: { // The Nokhud Offensive
+            id: 13982,
+            shortName: 'NO',
+        },
+        [401]: { // The Azure Vault
+            id: 13954,
+            shortName: 'AV',
+        },
+        [402]: { // Algeth'ar Academy
+            id: 14032,
+        },
+    },
+    // same as above, only by using the instance_map_id property
+    instanceOverride: {
+        [2522]: { // Vault of the Incarnates
+            id: 14030,
+            shortName: 'VOTI',
+        },
+        [2569]: { // Aberrus, the Shadowed Crucible
+            id: 14663,
+            shortName: 'ATSC',
         },
     },
 };
@@ -323,7 +363,7 @@ const config = {
     });
 
     dungeons.forEach(dungeon => {
-        const overrideData = config.keystoneInstanceOverride[dungeon.keystone_instance];
+        const overrideData = config.keystoneInstanceOverride[dungeon.keystone_instance] || config.instanceOverride[dungeon.instance_map_id];
         if (!overrideData)
             return;
         for (const key in overrideData)

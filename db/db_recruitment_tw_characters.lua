e92e799d1a56edadf8fb182fb3cc5e0ebb0fbbdd
Jah Raphael <jahraphael@yahoo.com>
2024-05-29 08:24:01 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-05-28T20:12:33Z",numCharacters=19139,db={}}
+local provider={name=...,data=3,region="tw",date="2024-05-29T08:17:54Z",numCharacters=18941,db={}}
 local F
 
 F = function() provider.db["世界之樹"]={0,"Qopjvathree"} end F()

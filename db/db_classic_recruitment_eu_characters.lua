d331af6a9c3c4c9d7c742874d97c75dc432ca4b6
Jah Raphael <jahraphael@yahoo.com>
2024-07-15 06:13:39 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,17 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-07-14T06:11:09Z",numCharacters=153,db={}}
+local provider={name=...,data=3,region="eu",date="2024-07-15T06:13:27Z",numCharacters=153,db={}}
 local F
 
 F = function() provider.db["Golemagg"]={0,"Alaudee","Auranapops","Auranapopz","Aurapop","Babyshorc","Bassmann","Beergrills","Blygkläppen","Boikee","Bulldogg","Buttshot","Clawedz","Demonicnipps","Diedzins","Dunamy","Dödsnö","Ehtype","Frostynipps","Grönakulor","Holynipps","Invisinipps","Jormaheart","Jormasoul","Kalukukkaro","Kanõ","Kanø","Legolazze","Lingle","Mellenis","Mordabullan","Mordachai","Moruga","Morugal","Morugaz","Muffindk","Myandu","Myduna","Myrasi","Mysari","Myudna","Myuli","Myunda","Mywifenow","Nooknook","Peregrinhobb","Pillaomekka","Pingle","Podzina","Priestgle","Qingle","Qingledk","Qinglemage","Qingletwo","Ramjam","Rooen","Rottenbeaver","Rupucis","Saldais","Sanza","Sauleszakis","Scorchie","Shmoedin","Shorcinator","Smeegs","Smêxy","Snorkka","Snovflake","Strandzhx","Strandzw","Thaelan","Xingle","Zarknesis","Zephyraxius","Zjaku","Zjakupala"} end F()
 F = function() provider.db["Auberdine"]={150,"Féanòr","Ivelnà","Molgonite","Ròllz"} end F()
 F = function() provider.db["Everlook"]={158,"Eulean","Keccs","Shantiie"} end F()
-F = function() provider.db["Gehennas"]={164,"Knutdkay","Maljik","Odintrap","Sagemaige","Sinblood","Sinlava","Sinwarr","ßlood"} end F()
-F = function() provider.db["Lakeshire"]={180,"Rizzina","Vulina"} end F()
+F = function() provider.db["Gehennas"]={164,"Knutdkay","Maljik","Nghtmr","Odintrap","Sagemaige","Sinblood","Sinlava","Sinwarr","ßlood"} end F()
+F = function() provider.db["Lakeshire"]={182,"Vulina"} end F()
 F = function() provider.db["Firemaw"]={184,"Amakab","Annat","Kamab"} end F()
 F = function() provider.db["PyrewoodVillage"]={190,"Alithien"} end F()
+F = function() provider.db["Razorfen"]={192,"Rändan"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

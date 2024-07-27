6449b3475fcd08713267d1b6e826d5c0f0a84b54
Jah Raphael <jahraphael@yahoo.com>
2024-07-27 06:14:06 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-07-26T06:12:54Z",numCharacters=143,db={}}
+local provider={name=...,data=3,region="eu",date="2024-07-27T06:12:53Z",numCharacters=140,db={}}
 local F
 
 F = function() provider.db["Lakeshire"]={0,"Aixai","Hanalka","Hildidin","Hildigardt","Hildíbrand","Lêf","Stormdancer","Vulina"} end F()
 F = function() provider.db["Everlook"]={16,"Eulean","Keccs","Shantiie"} end F()
 F = function() provider.db["Golemagg"]={22,"Alaudee","Babyshorc","Bassmann","Beergrills","Blygkläppen","Boikee","Bulldogg","Buttshot","Clawedz","Demonicnipps","Diedzins","Dirø","Dunamy","Dödsnö","Ehtype","Frostynipps","Grönakulor","Holynipps","Invisinipps","Jormaheart","Jormasoul","Justfrosty","Justfrostydk","Justfrostydr","Justfrostyh","Justfrostypl","Kalukukkaro","Kanõ","Kanø","Legolazze","Mellenis","Mordabullan","Mordachai","Moruga","Morugal","Morugaz","Myandu","Myduna","Myrasi","Mysari","Myudna","Myuli","Myunda","Mywifenow","Nooknook","Peregrinhobb","Pillaomekka","Podzina","Ramjam","Rooen","Rottenbeaver","Rupucis","Saldais","Sauleszakis","Scorchie","Shmoedin","Shorcinator","Smêxy","Snorkka","Snovflake","Thaelan","Zarknesis","Zephyraxius","Zjaku","Zjakupala"} end F()
-F = function() provider.db["Gehennas"]={152,"Cheor","Nghtmr","Sagemaige","Sinblood","Sinlava","Sinwarr"} end F()
-F = function() provider.db["Razorfen"]={164,"Rändan"} end F()
-F = function() provider.db["Sulfuron"]={166,"Cesca","Lexiluna"} end F()
-F = function() provider.db["PyrewoodVillage"]={170,"Ekolie"} end F()
+F = function() provider.db["Gehennas"]={152,"Cheor","Nghtmr","Sagemaige"} end F()
+F = function() provider.db["Razorfen"]={158,"Rändan"} end F()
+F = function() provider.db["Sulfuron"]={160,"Cesca","Lexiluna"} end F()
+F = function() provider.db["PyrewoodVillage"]={164,"Ekolie"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-07-26T06:12:54Z",numCharacters=143,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2024-07-27T06:12:53Z",numCharacters=140,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 114

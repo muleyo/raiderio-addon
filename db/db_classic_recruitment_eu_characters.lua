40c380d8572abaff647f3daf81c623722d107838
Jah Raphael <jahraphael@yahoo.com>
2024-08-22 06:37:32 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,12 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-08-21T06:15:24Z",numCharacters=130,db={}}
+local provider={name=...,data=3,region="eu",date="2024-08-22T06:36:20Z",numCharacters=73,db={}}
 local F
 
 F = function() provider.db["Lakeshire"]={0,"Aixai","Hanalka","Hildidin","Hildigardt","Hildíbrand","Lêf","Stormdancer"} end F()
-F = function() provider.db["Golemagg"]={14,"Alaudee","Babyshorc","Bassmann","Beergrills","Blygkläppen","Boikee","Bulldogg","Buttshot","Demonicnipps","Diedzins","Dunamy","Dödsnö","Ehtype","Frostynipps","Grönakulor","Holynipps","Invisinipps","Jormaheart","Jormasoul","Kalukukkaro","Kanõ","Kanø","Legolazze","Mellenis","Mordabullan","Mordachai","Moruga","Morugal","Morugaz","Myandu","Myduna","Myrasi","Mysari","Myudna","Myuli","Myunda","Mywifenow","Nooknook","Peregrinhobb","Pillaomekka","Podzina","Ramjam","Rooen","Rottenbeaver","Rupucis","Saldais","Sauleszakis","Scorchie","Shmoedin","Shorcinator","Smêxy","Snorkka","Snovflake","Zarknesis","Zephyraxius","Zjaku","Zjakupala"} end F()
-F = function() provider.db["Everlook"]={128,"Chùcknourish","Drskrt","Innoblaster","Skrtexecute","Skrtrog","Skrtsp","Tanovdk","Tanovdr","Tanovic"} end F()
-F = function() provider.db["Firemaw"]={146,"Lexapriest"} end F()
+F = function() provider.db["Everlook"]={14,"Chùcknourish","Drskrt","Innoblaster","Skrtexecute","Skrtrog","Skrtsp","Tanovdk","Tanovdr","Tanovic"} end F()
+F = function() provider.db["Firemaw"]={32,"Lexapriest"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-08-21T06:15:24Z",numCharacters=130,db={}}
+local provider={name=...,data=3,region="us",date="2024-08-22T06:36:20Z",numCharacters=73,db={}}
 local F
 
 F = function() provider.db["Grobbulus"]={0,"Alandia","Alice","Calivar","Calivardk","Calivarr","Calivarra","Calivartwo","Culmination","Disorders","Idalia","Idalie","Idari","Idaria","Imisscalivar","Imisslarker","Ixia","Katmai","Lapushka","Lillet","Lizocka","Macadamias","Molteenie","Molten","Moltengoat","Moltenqt","Natasha","Nith","Nsty","Nvsty","Poaching","Pulaundoh","Pulire","Shiftro","Southbeard","Tatsu","Testimony","Thiccivar","Tidder","Tidders","Vieliessar","Vistakia","Ytsan","Ytsanbald","Ytsandk"} end F()

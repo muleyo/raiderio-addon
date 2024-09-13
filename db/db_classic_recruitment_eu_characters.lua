85e25af871446962bbbdd593795467df6670d7f8
Jah Raphael <jahraphael@yahoo.com>
2024-09-13 06:14:17 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,12 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-09-12T06:14:14Z",numCharacters=15,db={}}
+local provider={name=...,data=3,region="eu",date="2024-09-13T06:14:04Z",numCharacters=13,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Daloon","Dýnem","Loneta","Sanshein","Taala","Veznik"} end F()
-F = function() provider.db["Gehennas"]={12,"Gillgahr"} end F()
-F = function() provider.db["Lakeshire"]={14,"Rizzina"} end F()
+F = function() provider.db["Lakeshire"]={12,"Rizzina"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

d5cb7e0a99d8914ff10866ca8743ecc0c3e3a6c1
Jah Raphael <jahraphael@yahoo.com>
2024-09-14 06:40:51 +0000

    [Raider.IO] Database Refresh

--- db/db_mythicplus_tw_characters.lua
+++ db/db_mythicplus_tw_characters.lua
@@ -2,17 +2,17 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=1,region="tw",date="2024-09-13T06:35:37Z",currentSeasonId=0,numCharacters=9238,keystoneMilestoneLevels={2,4,7,9,12,15},db={}}
+local provider={name=...,data=1,region="tw",date="2024-09-14T06:35:57Z",currentSeasonId=0,numCharacters=9641,keystoneMilestoneLevels={2,4,7,9,12,15},db={}}
 local F
 
 F = function() provider.db["米奈希爾"]={0,"Yamadk","Yamadope","Yamadruid","Yamaybmy","抹茶布丁貓","草莓優格貓","雪鹽焦糖貓"} end F()
 F = function() provider.db["憤怒使者"]={210,"Rymastr"} end F()
 F = function() provider.db["日落沼澤"]={240,"Iqoskk","Iqosll","Iqosrr","Pymastr","凝川","端鱗","雪淚","雲濤"} end F()
 F = function() provider.db["眾星之子"]={480,"龍蝦小魚卵"} end F()
-F = function() provider.db["水晶之刺"]={510,"後面有點癢","憨豆大媽","被強煎的蛋","西柚味鮮奶","麥能爺"} end F()
-F = function() provider.db["聖光之願"]={660,"憨豆大媽"} end F()
-F = function() provider.db["冰風崗哨"]={690,"菲蒙"} end F()
-F = function() provider.db["亞雷戈斯"]={720,"Bigdadnotail","老虎強"} end F()
+F = function() provider.db["亞雷戈斯"]={510,"Bigdadnotail","Ggone","老虎強"} end F()
+F = function() provider.db["水晶之刺"]={600,"後面有點癢","憨豆大媽","被強煎的蛋","西柚味鮮奶","麥能爺"} end F()
+F = function() provider.db["聖光之願"]={750,"憨豆大媽"} end F()
+F = function() provider.db["冰風崗哨"]={780,"菲蒙"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

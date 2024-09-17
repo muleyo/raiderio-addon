46815d879501700ef99fcf4427bb04e5aa1fa84a
Jah Raphael <jahraphael@yahoo.com>
2024-09-17 06:43:59 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-05-31T08:15:53Z",numCharacters=18730,db={}}
+local provider={name=...,data=3,region="tw",date="2024-09-17T06:43:03Z",numCharacters=23166,db={}}
 local F
 
-F = function() provider.db["世界之樹"]={0,"Qopjvathree"} end F()
+F = function() provider.db["眾星之子"]={0,"魘塵"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-05-31T08:15:53Z",numCharacters=18730,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2024-09-17T06:43:03Z",numCharacters=23166,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
-F = function() provider.lookup[1] = ";\4" end F()
+F = function() provider.lookup[1] = "\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

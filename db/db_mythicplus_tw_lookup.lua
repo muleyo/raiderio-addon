e6e9e726aee7f22379690756a55a1a77c0a95f3e
Jah Raphael <jahraphael@yahoo.com>
2024-09-18 06:52:43 +0000

    [Raider.IO] Database Refresh

--- db/db_mythicplus_tw_lookup.lua
+++ db/db_mythicplus_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=1,region="tw",date="2024-09-17T06:39:37Z",currentSeasonId=0,numCharacters=10886,keystoneMilestoneLevels={2,4,7,9,12,15},lookup={},recordSizeInBytes=30,encodingOrder={1,2,5,6,8,9,10,11,12,14}}
+local provider={name=...,data=1,region="tw",date="2024-09-18T06:46:57Z",currentSeasonId=0,numCharacters=124050,keystoneMilestoneLevels={2,4,7,9,12,15},lookup={},recordSizeInBytes=30,encodingOrder={1,2,5,6,8,9,10,11,12,14}}
 local F
 
 -- chunk size: 810
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-09-17T06:43:03Z",numCharacters=23166,db={}}
+local provider={name=...,data=3,region="tw",date="2024-09-18T06:51:20Z",numCharacters=23550,db={}}
 local F
 
 F = function() provider.db["眾星之子"]={0,"魘塵"} end F()

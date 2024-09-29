1cc9a95b620dadfea690a419a77e158045c5801b
Jah Raphael <jahraphael@yahoo.com>
2024-09-29 06:16:09 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,17 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-09-28T06:15:30Z",numCharacters=33,db={}}
+local provider={name=...,data=3,region="eu",date="2024-09-29T06:15:36Z",numCharacters=48,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Daloon","Dýnem","Loneta","Sanshein","Taala","Veznik"} end F()
 F = function() provider.db["Auberdine"]={12,"Brecknar","Dkpeurtermiq","Ghostdiablo","Healren","Tynagaratea"} end F()
-F = function() provider.db["Flamegor"]={22,"Ган","Дыкалыч","Киринн","Мыхалыч","Павуву","Пейнфула","Пейнфулаа","Пэйнфула","Пэйнфулла"} end F()
-F = function() provider.db["Firemaw"]={40,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Tigersclaw","Zaphyria","Zircona"} end F()
-F = function() provider.db["Lakeshire"]={54,"Rizzina"} end F()
-F = function() provider.db["Golemagg"]={56,"Yuts"} end F()
-F = function() provider.db["Sulfuron"]={58,"Paden"} end F()
+F = function() provider.db["Mandokir"]={22,"Choppeer","Explicita","Explicitø","Fendi","Gryphüs","Grÿphus","Moä","Rieh","Sylverwing","Titânia","Trankita","Trankïta","Trankös","Títania","Xplicitø"} end F()
+F = function() provider.db["Flamegor"]={52,"Ган","Дыкалыч","Киринн","Мыхалыч","Павуву","Пейнфула","Пейнфулаа","Пэйнфула","Пэйнфулла"} end F()
+F = function() provider.db["Firemaw"]={70,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Tigersclaw","Zaphyria","Zircona"} end F()
+F = function() provider.db["Lakeshire"]={84,"Rizzina"} end F()
+F = function() provider.db["Golemagg"]={86,"Yuts"} end F()
+F = function() provider.db["Sulfuron"]={88,"Paden"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-09-28T06:15:30Z",numCharacters=33,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2024-09-29T06:15:36Z",numCharacters=48,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 60
-F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\4\4\4\4\4\4" end F()
+-- chunk size: 90
+F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\13\10\13\10\13\10\13\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\4\4\4\4\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

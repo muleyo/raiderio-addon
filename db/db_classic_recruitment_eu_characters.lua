174849e48b9895268ad45d41a5a6de8be1a2d3d5
Jah Raphael <jahraphael@yahoo.com>
2024-10-06 06:16:16 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-10-05T06:15:48Z",numCharacters=51,db={}}
+local provider={name=...,data=3,region="eu",date="2024-10-06T06:15:33Z",numCharacters=52,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Daloon","Dýnem","Loneta","Sanshein","Taala","Veznik"} end F()
@@ -13,6 +13,7 @@ F = function() provider.db["Firemaw"]={68,"Arcusdruid","Husgris","Oranguboo","Or
 F = function() provider.db["Sulfuron"]={84,"Paden"} end F()
 F = function() provider.db["Gehennas"]={86,"Hairypowder","Zínet"} end F()
 F = function() provider.db["Golemagg"]={90,"Smiskey","Tovalina","Tovaliza"} end F()
+F = function() provider.db["Lakeshire"]={96,"Tâhrox"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-10-05T06:15:48Z",numCharacters=51,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2024-10-06T06:15:33Z",numCharacters=52,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 96
-F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\13\10\13\10\13\10\13\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\4\4\10\13\10\13\10\13\4\4\4\4\4\8\4\4\4\4\4\4" end F()
+-- chunk size: 98
+F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\13\10\13\10\13\10\13\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\10\13\4\4\10\13\10\13\10\13\4\4\4\4\4\8\4\4\4\4\4\4\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

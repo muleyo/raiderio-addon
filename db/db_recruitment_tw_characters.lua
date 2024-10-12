0696bfec64bda0be3cdaaea1133fc2a08ab5208c
Jah Raphael <jahraphael@yahoo.com>
2024-10-12 07:18:44 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-10-11T09:49:05Z",numCharacters=29910,db={}}
+local provider={name=...,data=3,region="tw",date="2024-10-12T07:12:42Z",numCharacters=30100,db={}}
 local F
 
 F = function() provider.db["眾星之子"]={0,"魘塵"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-10-11T09:49:05Z",numCharacters=29910,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2024-10-12T07:12:42Z",numCharacters=30100,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 6

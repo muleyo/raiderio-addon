35f997cb8c364180b33327a540df8e87dff19309
Jah Raphael <jahraphael@yahoo.com>
2024-10-14 06:16:23 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,17 +2,16 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-10-13T06:15:45Z",numCharacters=53,db={}}
+local provider={name=...,data=3,region="eu",date="2024-10-14T06:15:34Z",numCharacters=46,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Daloon","Dýnem","Loneta","Sanshein","Taala","Veznik"} end F()
 F = function() provider.db["Auberdine"]={12,"Brecknar","Dkpeurtermiq","Ghostdiablo","Healren","Tynagaratea"} end F()
 F = function() provider.db["Mandokir"]={22,"Choppeer","Explicita","Explicitø","Fendi","Gryphüs","Grÿphus","Moä","Rieh","Sylverwing","Titânia","Trankita","Trankïta","Trankös","Títania","Xplicitø"} end F()
-F = function() provider.db["Flamegor"]={52,"Ган","Дыкалыч","Киринн","Мыхалыч","Павуву","Пейнфула","Пейнфулаа","Пэйнфула","Пэйнфулла"} end F()
-F = function() provider.db["Firemaw"]={70,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Ornális","Tigersclaw","Zaphyria","Zircona"} end F()
-F = function() provider.db["Gehennas"]={86,"Hairypowder","Zínet"} end F()
-F = function() provider.db["Golemagg"]={90,"Smiskey","Tovalina","Tovaliza"} end F()
-F = function() provider.db["Lakeshire"]={96,"Tâhrox"} end F()
+F = function() provider.db["Firemaw"]={52,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Ornális","Tigersclaw","Zaphyria","Zircona"} end F()
+F = function() provider.db["Golemagg"]={68,"Hypatus","Smiskey","Tovalina","Tovaliza","Zigros"} end F()
+F = function() provider.db["Gehennas"]={78,"Hairypowder","Zínet"} end F()
+F = function() provider.db["Lakeshire"]={82,"Tâhrox"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-10-13T06:15:45Z",numCharacters=53,db={}}
+local provider={name=...,data=3,region="us",date="2024-10-14T06:15:34Z",numCharacters=46,db={}}
 local F
 
 F = function() provider.db["Arugal"]={0,"Careface","Chubs","Harrock"} end F()

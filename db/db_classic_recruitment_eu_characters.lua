75f566003073d77e44daa10220617ebd52072019
Jah Raphael <jahraphael@yahoo.com>
2024-10-15 06:17:26 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-10-14T06:15:34Z",numCharacters=46,db={}}
+local provider={name=...,data=3,region="eu",date="2024-10-15T06:16:28Z",numCharacters=44,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Daloon","Dýnem","Loneta","Sanshein","Taala","Veznik"} end F()
 F = function() provider.db["Auberdine"]={12,"Brecknar","Dkpeurtermiq","Ghostdiablo","Healren","Tynagaratea"} end F()
 F = function() provider.db["Mandokir"]={22,"Choppeer","Explicita","Explicitø","Fendi","Gryphüs","Grÿphus","Moä","Rieh","Sylverwing","Titânia","Trankita","Trankïta","Trankös","Títania","Xplicitø"} end F()
-F = function() provider.db["Firemaw"]={52,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Ornális","Tigersclaw","Zaphyria","Zircona"} end F()
-F = function() provider.db["Golemagg"]={68,"Hypatus","Smiskey","Tovalina","Tovaliza","Zigros"} end F()
-F = function() provider.db["Gehennas"]={78,"Hairypowder","Zínet"} end F()
-F = function() provider.db["Lakeshire"]={82,"Tâhrox"} end F()
+F = function() provider.db["Firemaw"]={52,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Tigersclaw","Zaphyria","Zircona"} end F()
+F = function() provider.db["Golemagg"]={66,"Hypatus","Smiskey","Tovalina","Tovaliza","Zigros"} end F()
+F = function() provider.db["Gehennas"]={76,"Hairypowder"} end F()
+F = function() provider.db["Lakeshire"]={78,"Tâhrox"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-10-14T06:15:34Z",numCharacters=46,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2024-10-15T06:16:28Z",numCharacters=44,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 8

26edd55dc2f67829df8624f2915fa6d0457f9ae6
Jah Raphael <jahraphael@yahoo.com>
2024-10-23 06:15:16 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-10-22T06:15:28Z",numCharacters=115,db={}}
+local provider={name=...,data=3,region="eu",date="2024-10-23T06:14:56Z",numCharacters=115,db={}}
 local F
 
 F = function() provider.db["Golemagg"]={0,"Acelle","Agresátor","Defiq","Derbicek","Derbino","Derblock","Grandios","Hypatus","Ironteon","Ironteonx","Irontheon","Mysllivec","Qetinka","Qetoslav","Shapy","Shapypally","Unmag","Watacarris","Zigros"} end F()

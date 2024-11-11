4a7f72b391ca06bd1bdca99d94ff4ec97b8c9e45
Jah Raphael <jahraphael@yahoo.com>
2024-11-11 06:10:08 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-10T06:09:37Z",numCharacters=118,db={}}
+local provider={name=...,data=3,region="us",date="2024-11-11T06:09:14Z",numCharacters=121,db={}}
 local F
 
 F = function() provider.db["Mankrik"]={0,"Ayahuascå","Ayå","Davinthian","Dethknel","Mayorsluv","Oakenhooves","Reeana","Reeath","Reeck","Reegue","Reeny","Screamzz","Steelhorns","Tassadór","Zulabiba"} end F()

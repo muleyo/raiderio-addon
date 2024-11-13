3bf1fd759f82e563c9d24af0e8981ebecee209b9
Jah Raphael <jahraphael@yahoo.com>
2024-11-13 06:09:56 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,19 +2,18 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-11-12T06:09:29Z",numCharacters=121,db={}}
+local provider={name=...,data=3,region="eu",date="2024-11-13T06:09:38Z",numCharacters=118,db={}}
 local F
 
-F = function() provider.db["Golemagg"]={0,"Acelle","Agresátor","Defiq","Derbicek","Derbino","Derblock","Grandios","Hypatus","Ironteon","Ironteonx","Irontheon","Mysllivec","Pivvo","Qetinka","Qetoslav","Shapy","Shapypally","Unmag","Watacarr","Zigros"} end F()
-F = function() provider.db["PyrewoodVillage"]={40,"Ambelin","Ambelintvå","Bearlydying","Bearsnout","Boredofbored","Boredofsword","Daethariel","Dystopatica","Ellandrieth","Esmdrood","Essemdk","Eszem","Ezm","Ezsem","Ezzem","Flumpadin","Gnomelie","Gnuttus","Hahadkbtw","Hahashamanxd","Hecel","Kinsym","Kynsim","Miskyn","Mordentha","Myksin","Myskin","Niksym","Pachamamie","Peteenjoyer","Petenjoyer","Sinkym","Sykmin","Syreczek","Tyrz","Tyrzs","Tyrzx","Warriorcoper","Xaistlol","Xaistmage","Xaistown","Xaistylol","Xaistymourne","Xaistyown","Xaistyr","Xaistytwo","Xaistyy","Xaizty"} end F()
-F = function() provider.db["MirageRaceway"]={136,"Adivoro","Ceillia","Deadrage","Deadseal","Jessih","Oxeen","Valerish","Zelline"} end F()
-F = function() provider.db["Everlook"]={152,"Anýpriest","Anýshus","Anýsteps","Cerciia","Díru","Huuthuut","Pogpüh","Shifthàppens","Swiggidy","Tizle"} end F()
-F = function() provider.db["Flamegor"]={172,"Джексколой","Келлибримбор","Чолик"} end F()
-F = function() provider.db["Firemaw"]={178,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Tigersclaw","Zaphyria","Zircona"} end F()
-F = function() provider.db["Mandokir"]={192,"Sylverwing"} end F()
-F = function() provider.db["Auberdine"]={194,"Druidille"} end F()
-F = function() provider.db["Sulfuron"]={196,"Hazas","Hazasham"} end F()
-F = function() provider.db["Gehennas"]={200,"Pâm","Tindk","Tinsham","Tínpriest"} end F()
+F = function() provider.db["Golemagg"]={0,"Acelle","Agresátor","Defiq","Derbicek","Derbino","Derblock","Grandios","Ironteon","Ironteonx","Irontheon","Mysllivec","Pivvo","Qetinka","Qetoslav","Shapy","Shapypally","Unmag","Watacarr"} end F()
+F = function() provider.db["PyrewoodVillage"]={36,"Ambelin","Ambelintvå","Bearlydying","Bearsnout","Boredofbored","Boredofsword","Daethariel","Dystopatica","Ellandrieth","Esmdrood","Essemdk","Eszem","Ezm","Ezsem","Ezzem","Flumpadin","Gnomelie","Gnuttus","Hahadkbtw","Hahashamanxd","Hecel","Kinsym","Kynsim","Miskyn","Mordentha","Myksin","Myskin","Niksym","Pachamamie","Peteenjoyer","Petenjoyer","Sinkym","Sykmin","Syreczek","Tyrz","Tyrzs","Tyrzx","Warriorcoper","Xaistlol","Xaistmage","Xaistown","Xaistylol","Xaistymourne","Xaistyown","Xaistyr","Xaistytwo","Xaistyy","Xaizty"} end F()
+F = function() provider.db["MirageRaceway"]={132,"Adivoro","Ceillia","Deadrage","Deadseal","Jessih","Oxeen","Valerish","Zelline"} end F()
+F = function() provider.db["Everlook"]={148,"Cerciia","Díru","Huuthuut","Pogpüh","Shifthàppens","Swiggidy","Tizle"} end F()
+F = function() provider.db["Flamegor"]={162,"Джексколой","Келлибримбор","Чолик"} end F()
+F = function() provider.db["Firemaw"]={168,"Arcusdruid","Husgris","Oranguboo","Oranguroo","Tigersclaw","Zaphyria","Zircona"} end F()
+F = function() provider.db["Mandokir"]={182,"Sylverwing"} end F()
+F = function() provider.db["Sulfuron"]={184,"Hazas","Hazasham","Shyyz"} end F()
+F = function() provider.db["Gehennas"]={190,"Jijelt","Pâm","Tindk","Tinsham","Tínpriest","Zöega"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

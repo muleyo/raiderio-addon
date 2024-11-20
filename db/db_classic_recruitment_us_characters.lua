644c5fcf62d0c071084c1fe7cbd671185966b6d7
Jah Raphael <jahraphael@yahoo.com>
2024-11-20 06:10:29 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,11 +2,10 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-19T06:09:00Z",numCharacters=52,db={}}
+local provider={name=...,data=3,region="us",date="2024-11-20T06:09:26Z",numCharacters=44,db={}}
 local F
 
 F = function() provider.db["Mankrik"]={0,"Ayahuascå","Ayå","Blazem","Davinthian","Dethknel","Galacnocok","Grukori","Kadron","Mayorsluv","Message","Oakenhooves","Reeana","Reeath","Reeck","Reegue","Reeny","Screamzz","Steelhorns","Styggmz","Styggz","Tassadór","Tyder","Tydesin","Tydison","Weast","Zulabiba"} end F()
-F = function() provider.db["Faerlina"]={52,"Sugarmojo"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-19T06:09:00Z",numCharacters=52,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2024-11-20T06:09:26Z",numCharacters=44,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 54
-F = function() provider.lookup[1] = "\10\5\10\5\10\21\10\5\10\5\10\21\10\21\10\21\10\5\10\21\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\21\10\21\10\5\10\21\10\21\10\21\10\21\10\5\4\4" end F()
+-- chunk size: 52
+F = function() provider.lookup[1] = "\10\5\10\5\10\21\10\5\10\5\10\21\10\21\10\21\10\5\10\21\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\21\10\21\10\5\10\21\10\21\10\21\10\21\10\5" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

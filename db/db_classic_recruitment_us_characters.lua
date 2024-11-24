5a0a92da3214db76b5e96f73ff6fd29d874ccf91
Jah Raphael <jahraphael@yahoo.com>
2024-11-24 06:17:56 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,11 +2,12 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-23T06:09:33Z",numCharacters=54,db={}}
+local provider={name=...,data=3,region="us",date="2024-11-24T06:17:14Z",numCharacters=56,db={}}
 local F
 
 F = function() provider.db["Whitemane"]={0,"Gsm","Gunpowder","Jadey","Tramclam","Vrokar"} end F()
 F = function() provider.db["Mankrik"]={10,"Ayahuascå","Ayå","Blazem","Davinthian","Dethknel","Galacnocok","Grukori","Kadron","Mayorsluv","Message","Oakenhooves","Reeana","Reeath","Reeck","Reegue","Reeny","Screamzz","Steelhorns","Styggmz","Styggz","Tassadór","Tyder","Tydesin","Tydison","Weast","Zulabiba"} end F()
+F = function() provider.db["Atiesh"]={62,"Immadk","Immaheal"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-23T06:09:33Z",numCharacters=54,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2024-11-24T06:17:14Z",numCharacters=56,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 62
-F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\5\10\5\10\21\10\5\10\5\10\21\10\21\10\21\10\5\10\21\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\21\10\21\10\5\10\21\10\21\10\21\10\21\10\5" end F()
+-- chunk size: 66
+F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\5\10\5\10\21\10\5\10\5\10\21\10\21\10\21\10\5\10\21\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\21\10\21\10\5\10\21\10\21\10\21\10\21\10\5\4\4\4\8" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)

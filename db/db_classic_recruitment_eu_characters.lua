f6e2c301611bf3f35605d9c2d31ab5002ba0921c
Jah Raphael <jahraphael@yahoo.com>
2024-11-29 06:09:49 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-11-28T06:09:05Z",numCharacters=57,db={}}
+local provider={name=...,data=3,region="eu",date="2024-11-29T06:08:52Z",numCharacters=57,db={}}
 local F
 
 F = function() provider.db["PyrewoodVillage"]={0,"Ambelin","Ambelintvå","Bearsnout","Daethariel","Dystopatica","Elethia","Ellandrieth","Gnomelie","Gnuttus","Ludidoktor","Mordentha","Noshecant","Paliox","Psyvise","Syreczek","Wariox"} end F()

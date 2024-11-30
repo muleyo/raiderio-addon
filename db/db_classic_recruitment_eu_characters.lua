1a80f7086f40e6a0e31fe61fbe8cb46cd40d9de6
Jah Raphael <jahraphael@yahoo.com>
2024-11-30 06:09:13 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,14 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2024-11-29T06:08:52Z",numCharacters=57,db={}}
+local provider={name=...,data=3,region="eu",date="2024-11-30T06:08:55Z",numCharacters=64,db={}}
 local F
 
 F = function() provider.db["PyrewoodVillage"]={0,"Ambelin","Ambelintvå","Bearsnout","Daethariel","Dystopatica","Elethia","Ellandrieth","Gnomelie","Gnuttus","Ludidoktor","Mordentha","Noshecant","Paliox","Psyvise","Syreczek","Wariox"} end F()
 F = function() provider.db["Flamegor"]={32,"Джексколой","Келлибримбор","Чолик"} end F()
 F = function() provider.db["Mandokir"]={38,"Sylverwing"} end F()
 F = function() provider.db["Firemaw"]={40,"Bluntsy","Skruxy"} end F()
+F = function() provider.db["Golemagg"]={44,"Justfrosty","Justfrostydd","Justfrostydk","Justfrostydr","Justfrostyh","Justfrostypl","Shamikazze"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2024-11-29T06:08:52Z",numCharacters=57,db={}}
+local provider={name=...,data=3,region="us",date="2024-11-30T06:08:55Z",numCharacters=64,db={}}
 local F
 
 F = function() provider.db["Whitemane"]={0,"Gsm","Gunpowder","Jadey","Tramclam","Vrokar"} end F()

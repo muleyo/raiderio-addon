0f442850cd35884ed5d9bdde10e04156d8e5a947
Jah Raphael <jahraphael@yahoo.com>
2024-12-10 07:27:07 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2024-12-09T07:20:33Z",numCharacters=30837,db={}}
+local provider={name=...,data=3,region="kr",date="2024-12-10T07:18:49Z",numCharacters=30985,db={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"Luntik","Naussea","Plastelin","Xyu"} end F()
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-12-09T07:20:33Z",numCharacters=30837,db={}}
+local provider={name=...,data=3,region="tw",date="2024-12-10T07:18:49Z",numCharacters=30985,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Jackj"} end F()
